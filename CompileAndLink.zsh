b7f76945d0c6fc1e6d2c97d25352155dcb490049
Andrea Cacioli <andrea.cacioli.1c@gmail.com>
2020-11-02 12:07:07 +0100

    version 1.0
    Version 1.0 of the CompileAndLink Project

---
+++ CompileAndLink.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+gcc -c ./*.c
+EXECUTABLE=$1
+LIBNAME=$2
+for f in *.o
+  do
+  if [ "$f" != "${EXECUTABLE}.o" ]
+  then
+  echo "Adding $f to lib${LIBNAME}.a"
+  ar r lib${LIBNAME}.a $f
+  fi
+  done
+ranlib lib${LIBNAME}.a
+cc ${EXECUTABLE}.o -L. -l${LIBNAME} -o ${EXECUTABLE}
